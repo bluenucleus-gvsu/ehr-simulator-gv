4858238efe2116f41943eaef11871b913b65069f
Max Mulder <maxmulder03@gmail.com>
2026-03-25 18:05:54 -0400

    Fix incorrect merge conflict resolutions

--- ehr-simulator/src/actions/cases.ts
+++ ehr-simulator/src/actions/cases.ts
@@ -119,7 +119,7 @@ export async function getCaseByCourseId(id: string) {
 export async function getSectionCaseAssignments(courseId: string) {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
   const { data, error } = await supabase
