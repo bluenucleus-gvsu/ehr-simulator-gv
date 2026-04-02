6ecf87b7c55f95b729d9bf1946ad84acafc004a1
Max Mulder <maxmulder03@gmail.com>
2026-04-02 11:03:00 -0400

    nit - workaround for user redirection occasionally failing

--- ehr-simulator/src/app/page.tsx
+++ ehr-simulator/src/app/page.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { createServerSupabase } from "@/utils/supabase/server";
 
 export default async function Home() {
 
+  const supabase = await createServerSupabase();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const id = user?.id;
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Link href="/simulation/123/chart/overview" passHref className="p-4">
-        <Button>EHR Simulation Demo</Button>
+      <Link href={`/user/profile/${id}`} passHref className="p-4">
+        <Button>My Profile</Button>
       </Link>
       <Link href="/admin" passHref>
         <Button>Admin Portal</Button>
